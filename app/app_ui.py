d1f4b0e6dd1a287c806d1bdf466bd302732bedd6
Nguyen Tran Thong <thong0710@users.noreply.huggingface.co>
2025-04-23 15:18:12 +0000

    Update UI part5

--- app/app_ui.py
+++ app/app_ui.py
@@ -51,27 +51,25 @@ with gr.Blocks(title="SAIFGuard - GENAI HỖ TRỢ PHÁT HIỆN PROMPT & IMAGE K
     
     with gr.Tab("📝 Kiểm duyệt Prompt"):
         with gr.Row():
-            # Cột bên trái cho status, output và button
+            with gr.Column(scale=1):
+                prompt_input = gr.Textbox(label="Nhập Prompt", lines=2)
+                
             with gr.Column(scale=1):
                 prompt_status = gr.Textbox(label="Trạng thái kiểm duyệt")
                 prompt_output = gr.Textbox(label="Kết quả GenAI")
                 prompt_button = gr.Button("Kiểm tra Prompt", elem_classes="yellow-btn")
             
-            # Cột bên phải cho input
-            with gr.Column(scale=1):
-                prompt_input = gr.Textbox(label="Nhập Prompt", lines=10)
         
         prompt_button.click(handle_prompt, inputs=prompt_input, outputs=[prompt_status, prompt_output])
     
     with gr.Tab("🖼️ Kiểm duyệt Hình ảnh"):
         with gr.Row():
-            # Cột bên trái cho output và button
+            # Cột bên trái
+            with gr.Column(scale=1):
+                image_input = gr.Image(type="pil", label="Tải ảnh lên")
+            # Trái
             with gr.Column(scale=1):
                 image_output = gr.Textbox(label="Trạng thái kiểm duyệt hình ảnh")
                 image_button = gr.Button("Kiểm tra Hình ảnh", elem_classes="yellow-btn")
-            
-            # Cột bên phải cho image input
-            with gr.Column(scale=1):
-                image_input = gr.Image(type="pil", label="Tải ảnh lên")
                 
         image_button.click(fn=check_image_nsfw, inputs=image_input, outputs=image_output)
