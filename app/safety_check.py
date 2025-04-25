78ce5db395973651e5e46bb83c609b86f5bc0f70
Nguyen Tran Thong <thong0710@users.noreply.huggingface.co>
2025-04-25 13:36:34 +0000

    Update app/safety_check.py

--- app/safety_check.py
+++ app/safety_check.py
@@ -47,7 +47,7 @@ def check_image_safe(image: Image.Image):
     violence_label = violence_labels[violence_pred]
     violence_score = violence_probs[violence_pred].item() * 100
 
-    if violence_label.lower() in ["violent", "violence", "LABEL_0"] and violence_score > 50:
+    if violence_label.lower() in ["violent", "violence", "LABEL_0","LABEL_1"] and violence_score > 50:
         return f"🚨 Ảnh KHÔNG an toàn:\n- Ảnh chứa bạo lực ({violence_label} - {violence_score:.2f}%)"
 
     return f"✅ Ảnh an toàn\n- NSFW: {nsfw_label} ({nsfw_score:.2f}%)\n- Violence: {violence_label} ({violence_score:.2f}%)"
